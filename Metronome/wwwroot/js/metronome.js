8a4d76be3900d7dba621fcee8c26b695e7e83e89
tosumi baseball <tosumi.baseball.videos@gmail.com>
2026-07-26 12:51:57 +0900

    phase 2 updates

--- Metronome/wwwroot/js/metronome.js
+++ Metronome/wwwroot/js/metronome.js
@@ -1,26 +1,38 @@
 ﻿window.metronome = {
 
+
     timer: null,
 
     bpm: 85,
 
+    beat: 0,
+
+    beatsPerMeasure: 4,
+
+    volume: 0.5,
+
     audioContext: null,
 
 
-    start: function (bpm) {
+    start: function (bpm, beats, volume) {
 
         this.bpm = bpm;
 
+        this.beatsPerMeasure = beats;
+
+        this.volume = volume;
+
+
         this.stop();
+
 
         if (!this.audioContext) {
             this.audioContext =
-                new (window.AudioContext ||
-                    window.webkitAudioContext)();
+                new AudioContext();
         }
 
-        this.timer = setInterval(() => {
 
+        this.timer = setInterval(() => {
             this.tick();
 
         }, 60000 / this.bpm);
@@ -28,38 +40,62 @@
     },
 
 
+
     stop: function () {
 
-        if (this.timer) {
+        clearInterval(this.timer);
 
-            clearInterval(this.timer);
-            this.timer = null;
+        this.timer = null;
 
-        }
+        this.beat = 0;
 
     },
+
 
 
     setTempo: function (bpm) {
-
         this.bpm = bpm;
 
-        if (this.timer) {
-
-            this.start(bpm);
-
-        }
-
+        if (this.timer)
+            this.start(
+                this.bpm,
+                this.beatsPerMeasure,
+                this.volume);
     },
+
+
+
+    setBeatsPerMeasure: function (value) {
+        this.beatsPerMeasure = value;
+    },
+
+
+
+    setVolume: function (value) {
+        this.volume = value;
+    },
+
 
 
     tick: function () {
 
+        this.beat++;
+
+
+        if (this.beat >
+            this.beatsPerMeasure) {
+            this.beat = 1;
+        }
+
+
         this.animateBeat();
 
-        this.playClick();
+
+        this.playClick(
+            this.beat === 1);
 
     },
+
 
 
     animateBeat: function () {
@@ -67,86 +103,50 @@
         let beat =
             document.querySelector(".beat");
 
-        if (beat) {
 
+        if (beat) {
             beat.classList.remove("active");
 
             void beat.offsetWidth;
 
             beat.classList.add("active");
-
         }
 
     },
 
 
-    playClick: function () {
 
-        if (!this.audioContext)
-            return;
+    playClick: function (accent) {
 
-
-        let oscillator =
+        let osc =
             this.audioContext.createOscillator();
+
 
         let gain =
             this.audioContext.createGain();
 
 
-        oscillator.frequency.value = 1200;
 
-        gain.gain.value = 0.15;
+        osc.frequency.value =
+            accent ? 1600 : 1000;
 
 
-        oscillator.connect(gain);
+        gain.gain.value =
+            this.volume;
+
+
+
+        osc.connect(gain);
 
         gain.connect(
             this.audioContext.destination);
 
 
-        oscillator.start();
 
-        oscillator.stop(
-            this.audioContext.currentTime + 0.05);
+        osc.start();
 
-    },
-
-
-    registerKeyboard: function (dotnet) {
-
-        document.addEventListener(
-            "keydown",
-            function (e) {
-
-
-                if (e.code === "Space") {
-
-                    e.preventDefault();
-
-                    dotnet.invokeMethodAsync(
-                        "KeyboardToggle");
-
-                }
-
-
-                if (e.code === "ArrowUp") {
-
-                    dotnet.invokeMethodAsync(
-                        "KeyboardTempoChange",
-                        1);
-
-                }
-
-
-                if (e.code === "ArrowDown") {
-
-                    dotnet.invokeMethodAsync(
-                        "KeyboardTempoChange",
-                        -1);
-
-                }
-
-            });
+        osc.stop(
+            this.audioContext.currentTime + .05);
 
     }
 
